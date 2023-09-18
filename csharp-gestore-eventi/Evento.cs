a587ea359920d61c6881cd9dc34b24c0318e9c52
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-09-18 10:54:14 +0200

    MileStone 1 con errori criptici

---
+++ csharp-gestore-eventi/Evento.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class Evento
+    {
+        // PROPRIETA'
+        public string Titolo { get; set; }
+        public DateTime Data { get; set; }
+        public int CapienzaMassima { get; }
+        public int PostiPrenotati { get; }
+    }
+
+    // COSTRUTTORE
+    public Evento(string titolo, DateTime data, int capienzaMassima, int postiPrenotati)
+    {
+        this.Titolo = titolo;
+        this.Data = data;
+        this.CapienzaMassima = capienzaMassima;
+        this.PostiPrenotati = postiPrenotati;
+    }
+
+    //GETTER 
+    public string MostraTitolo(string titolo)
+    {
+        return this.Titolo;
+    }
+
+    public DateTime MostraData(DateTime data)
+    {
+        return this.Data;
+    }
+
+    public int MostraCapienza(int capienzaMassima)
+    {
+        return this.CapienzaMassima;
+    }
+
+    public interface MostraPosti(int postiPrenotati)
+    {
+        ReturnTypeEncoder this.PostiPrenotati;
+    }
+}
